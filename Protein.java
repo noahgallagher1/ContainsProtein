caa6b1cbf304f600f9dd6050cc22aab316ed4e21
noahgallagher1 <61672116+noahgallagher1@users.noreply.github.com>
2020-08-21 15:05:43 -0700

    Update Protein.java

--- Protein.java
+++ Protein.java
@@ -1,19 +1,15 @@
 public class Protein {
     //this program determins if there is a protein in a strand of DNA
     public static void main(String[] args){
+        //Three random DNA strands to test.
         String dna1 = "ATGCGATACGCTTGA";
         String dna2 = "ATGCGATACGTGA";
         String dna3 = "ATTAATATGTACTGA";
+        
+        //Choose which DNA strand you want to analyze here.
         String dna = dna3;
 
-        //System.out.println(dna.length());
-        //System.out.println(dna.indexOf("TGA"));
-        //int start = dna.indexOf("A");
-        //int end = dna.length();
-        //int startEnd = dna.length()-2;
-        //System.out.println(dna.indexOf("ATG"));
-        //System.out.println(dna.indexOf("ATG")+3);
-
+       
         if (dna.substring(dna.indexOf("ATG"),dna.indexOf("ATG")+3).equals("ATG") && dna.substring(dna.indexOf("TGA"),dna.indexOf("TGA")+3).equals("TGA") && dna.length() % 3 == 0){
             System.out.println("This is a protein.");
         }
